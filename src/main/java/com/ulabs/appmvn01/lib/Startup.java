8452a5eb830a6e0681cb01e7f6bb81d74040d4b4
mguarniz <meguarnizc@indracompany.com>
2024-09-09 01:07:48 -0500

    design ok for login

---
+++ src/main/java/com/ulabs/appmvn01/lib/Startup.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ulabs.appmvn01.lib;
+
+import com.ulabs.appmvn01.application.Login2;
+
+/**
+ *
+ * @author manuelguarniz
+ */
+public class Startup {
+    public static void main(String[] args) {
+        
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+                Login2 login = new Login2();
+                login.setVisible(true);
+                login.setLocationRelativeTo(null);
+//            }
+//        });
+    }
+}
